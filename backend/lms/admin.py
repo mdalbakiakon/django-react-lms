2bed96a1634c30904a5952fe40136d441f3adc22
Md. Al Baki Akon <shahjadsiam512@gmail.com>
2026-01-31 12:06:18 +0600

    Fresh Start

--- backend/lms/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Category, Course, Enrollment
-
-admin.site.register(Category)
-admin.site.register(Course)
-admin.site.register(Enrollment)
